34ea4fe221f50bae52e1c9dfeca102c15d37a991
kxfeng <kxfchn@gmail.com>
2018-07-28 16:16:34 +0800

    add sample app

---
+++ sample/src/main/java/com/github/kxfeng/securepreferences/App.java
@@ -0,0 +1,33 @@
+package com.github.kxfeng.securepreferences;
+
+import android.app.Application;
+import android.content.SharedPreferences;
+
+import javax.crypto.SecretKey;
+
+public class App extends Application {
+
+    private static App INSTANCE = null;
+
+    private SharedPreferences securePreferences = null;
+
+    public static App getInstance() {
+        return INSTANCE;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        INSTANCE = this;
+
+        AppKeyStore.init(this);
+
+        SecretKey aesKey = AppKeyStore.getOrCreateSecretKey("SECURE_PREFERENCES_KEY_ALIAS", "AES", 256);
+        securePreferences = new SecurePreferences(this, "app_secure_preferences", aesKey);
+    }
+
+    public SharedPreferences getSecurePreferences() {
+        return securePreferences;
+    }
+}
--- sample/src/main/java/com/github/kxfeng/securepreferences/MainActivity.java
+++ sample/src/main/java/com/github/kxfeng/securepreferences/MainActivity.java
@@ -1,13 +1,33 @@
 package com.github.kxfeng.securepreferences;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editKey = null;
+    private EditText editValue = null;
+    private TextView textResult = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editKey = findViewById(R.id.edit_key);
+        editValue = findViewById(R.id.edit_value);
+        textResult = findViewById(R.id.text_result);
+
+        findViewById(R.id.btn_set).setOnClickListener(v -> {
+            App.getInstance().getSecurePreferences().edit().putString(editKey.getText().toString(), editValue.getText().toString()).apply();
+        });
+
+        findViewById(R.id.btn_get).setOnClickListener(v -> {
+            String key = editKey.getText().toString();
+            String value = App.getInstance().getSecurePreferences().getString(key, null);
+            textResult.setText("key=" + key + ", value=" + value);
+        });
     }
 }
